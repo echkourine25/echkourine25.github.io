95c15887d9a2037e467a233b1ed1a73100b87acf
echkourine25 <61584703+echkourine25@users.noreply.github.com>
2023-09-24 08:47:46 +0200

    Update init.js

--- init.js
+++ init.js
@@ -1,12 +1,5 @@
 const grid=document.getElementById("gridContainer");
 grid.autofocus;
-const audioElem = document.getElementById("myAudio");
-audioElem.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, false);
-audioElem.play();
-
 generateNewTopNumber();
 drawGrid();
 window.addEventListener("keydown", handleInput);
