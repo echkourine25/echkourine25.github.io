2cbae60c615ab45d6d3b73c909979c242d30b736
echkourine25 <echkourine@icloud.com>
2023-12-14 12:19:10 +0100

    test

--- draw.js
+++ draw.js
@@ -1,128 +1,127 @@
-<!DOCTYPE html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="generator" content="RocketCake"><title></title></head><style>a { color:#5D5D9E; } a:visited { color:#5D5D9E; } a:active { color:#5C615E; } a:hover { color:#B2B3B4; } a.wsp48fac406{ color:#0080FF; text-decoration: none} a.wsp48fac406:visited{ color:#0080FF; text-decoration: none} a.wsp48fac406:active{ color:#8018CB; text-decoration: underline} a.wsp48fac406:hover{ color:#0000A0; text-decoration: underline} a.wsp63851a2{ color:#FFFFFF; text-decoration: none} a.wsp63851a2:visited{ color:#FFFFFF; text-decoration: none} a.wsp63851a2:active{ color:#FFFFFF; text-decoration: underline} a.wsp63851a2:hover{ color:#C0C0C0; text-decoration: underline} body { background-color:#FFFFFF; padding:0;  margin: 0; }.textstyle1 { text-align:left; }.textstyle2 { font-size:12pt; font-family:Arial, Helvetica, sans-serif; color:#000000;  }.textstyle3 { text-align:center; }#container_78bff89b { vertical-align: top; position:relative; display: inline-block; width:100%; min-height:150px; max-width:700px; background:none;  }#container_78bff89b_padding { margin: 30px; display: block;  }.textstyle4 { font-size:12pt; font-family:Arial, Helvetica, sans-serif; color:#000000; line-height: 1.5;  }.textstyle5 { font-size:22pt; font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; color:#303030; line-height: 1.5;  }.textstyle6 { font-size:12pt; font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; color:#000000; line-height: 1.5;  }.textstyle7 { font-size:12pt; font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; color:#303030; line-height: 1.5;  }#container_365f079f { vertical-align: top; position:relative; display: inline-block; width:100%; min-height:26px; background-color:#C0C0C0;  }#container_365f079f_padding { margin: 10px; display: block;  }.textstyle8 { text-align:right; }.textstyle9 { font-size:10pt; font-family:Arial, Helvetica, sans-serif; color:#1D1D1D;  }</style><body><div class="textstyle1"><span class="textstyle2">  </span></div><div class="textstyle3"><div id="container_78bff89b"><div id="container_78bff89b_padding" ><div class="textstyle3"><span class="textstyle4"><br/><br/></span><span class="textstyle5">Encrypted Website</span><span class="textstyle6"><br/><br/>Your browser actually cannot show this page.</span><span class="textstyle7"><br/><br/></span><span class="textstyle7"><a href="mailto:echkourine@icloud.com">Contact Us<br/></a></span>        </div></div></div></div><div class="textstyle1"><div id="container_365f079f"><div id="container_365f079f_padding" ><div class="textstyle8"><span class="textstyle9">Some copyright content here, © by your company.</span></div></div></div></div></body><div style="display: none;">
+function drawNextNumber(){
+    const nextNumberElement = document.getElementById('nextNumber');
+    nextNumberElement.innerHTML = "<h3>Next: "+topNewNumberValue+"</h3>";
+    console.log("nextNumber drawn");
+}
+function drawGrid() {
+if(inGame){
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    for (let row = 0; row < gridHeight; row++) {
+        console.log("gridHeight="+gridHeight);
+        for (let col = 0; col < gridSize; col++) {
+            console.log("gridSize="+gridSize);
+            const x = col * rectSize;
+            const y = row * rectSize;
 
-/*}
-}    
-etelpmoc si noitamina eht retfa rebmun pot wen a etareneG // ;)(rebmuNpoTweNetareneg        
-{ esle }    
-;))xednIdne ,xednItxen(nwoDevoMetamina >= )((emarFnoitaminAtseuqer        
-{ ))xednIdne > xednItxen && 1- === noitcerid( || )xednIdne < xednItxen && 1 === noitcerid(( fi    
-xedni noitanitsed eht gnihcaer litnu noitamina eht eunitnoC //    
+            const number = numbers[row * gridSize + col];
+            if (number !== undefined && (typeof number === "number" && number >= 1 && number <= 7)) {
+                ctx.beginPath();
+                ctx.arc(x + rectSize / 2, y + rectSize / 2, rectSize / 2, 0, 2 * Math.PI);
+                ctx.fillStyle = "gainsboro";
+                ctx.fill();
+            }
+            else if (number === "=" || number === "-") {
+                ctx.beginPath();
+                ctx.arc(x + rectSize / 2, y + rectSize / 2, rectSize / 2, 0, 2 * Math.PI);
+                ctx.fillStyle = "gainsboro";
+                ctx.fill();
+            }
 
-;)(dirGward    
-;denifednu = ]xednItrats[srebmun    
-;eulaVrebmuNpot = ]xednItxen[srebmun    
-xedni txen eht ot rebmun pot eht evoM //    
-
-;deepSnoitamina * noitcerid + xednItrats = xednItxen tsnoc    
-
-pu gnivom rof 1- ,nwod gnivom rof 1 // ;1- : 1 ? 0 > ecnatsid = noitcerid tsnoc    
-;xednItrats - xednIdne = ecnatsid tsnoc    
-tnemevom fo deeps eht lortnoc ot eulav siht tsujdA // ;5 = deepSnoitamina tsnoc    
-{ )xednIdne ,xednItrats(nwoDevoMetamina noitcnuf
+            if (number !== undefined) {
+                ctx.fillStyle = (typeof number === "number") ? colors[number - 1] : "red";
+                ctx.font = "20px Arial";
+                ctx.textAlign = "center";
+                ctx.textBaseline = "middle";
+                ctx.fillText(number.toString(), x + rectSize / 2, y + rectSize / 2);
+            }
+        }
+    }
+    drawNextNumber();
 
 }
-}    
-etelpmoc si noitamina eht retfa rebmun pot wen a etareneG // ;)(rebmuNpoTweNetareneg        
-{ esle }    
-;))xednIdne ,xednItxen(nwoDevoMetamina >= )((emarFnoitaminAtseuqer        
-{ ))xednIdne > xednItxen && 1- === noitcerid( || )xednIdne < xednItxen && 1 === noitcerid(( fi    
-xedni noitanitsed eht gnihcaer litnu noitamina eht eunitnoC //    
+else if(mainScreen){
+const ctx = canvas.getContext("2d");
 
-;)(dirGward    
-;denifednu = ]xednItrats[srebmun    
-;eulaVrebmuNpot = ]xednItxen[srebmun    
-xedni txen eht ot rebmun pot eht evoM //    
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-;deepSnoitamina * noitcerid + xednItrats = xednItxen tsnoc    
-
-pu gnivom rof 1- ,nwod gnivom rof 1 // ;1- : 1 ? 0 > ecnatsid = noitcerid tsnoc    
-;xednItrats - xednIdne = ecnatsid tsnoc    
-tnemevom fo deeps eht lortnoc ot eulav siht tsujdA // ;5 = deepSnoitamina tsnoc    
-{ )xednIdne ,xednItrats(nwoDevoMetamina noitcnuf
-*/
-}
-;)002 ,nwoDevom(tuoemiTtes//    
-yaled trohs a retfa noitamina eht eunitnoC //    
-
-;)(dirGward    
-;denifednu = ]xednIrebmuNpot[srebmun    
-;eulaVrebmuNpot = ]xednInoitanitsed[srebmun    
-)nmuloc tegrat eht ot nwod( xedni txen eht ot rebmun pot eht evoM //    
-
-}    
-;eziSdirg =+ xednInoitanitsed        
-{ )denifednu === ]eziSdirg + xednInoitanitsed[srebmun && slleClatot < eziSdirg + xednInoitanitsed( elihw    
-;xednIrebmuNpot = xednInoitanitsed tel    
-nmuloc tegrat eht ni nwod wor eno evom ot rebmun pot eht rof xedni noitanitsed eht etaluclaC //    
-
-}    
-;nruter        
-llec dellif a gniretnuocne ro mottob eht gnihcaer retfa rebmun pot wen a etareneG // ;)(rebmuNpoTweNetareneg//        
-{ )delliFwoleBlleCsi || woRmottoBtAsi( fi    
-woleb llec dellif a si ereht ro wor mottob eht ta si rebmun pot eht fi nwod gnivom potS //    
-
-;denifednu ==! ]xednIwoleBllec[srebmun = delliFwoleBlleCsi tsnoc    
-;eziSdirg + xednIrebmuNpot = xednIwoleBllec tsnoc    
-;eziSdirg * )1 - thgieHdirg( => xednIrebmuNpot = woRmottoBtAsi tsnoc    
-woleb llec dellif a si ereht ro wor mottob eht ta si rebmun pot eht fi kcehC //    
-
-;)eulaVrebmuNpot(fOxedni.srebmun = xednIrebmuNpot tsnoc    
-yarra srebmun eht ni rebmun pot eht fo xedni eht dniF //    
-{ )(nwoDevom noitcnuf
-
+ctx.font = "20px Arial";
+ctx.fillStyle = "lightgreen";
+ctx.textAlign = "center";
+ctx.fillText("Press \"Down\" to Start !", canvas.width/2, canvas.height/2);
+mainScreen=false;
 }
 }
-;eslaf=neercSniam
-;)2/thgieh.savnac ,2/htdiw.savnac ,"! tratS ot "\nwoD"\ sserP"(txeTllif.xtc
-;"retnec" = ngilAtxet.xtc
-;"neergthgil" = elytSllif.xtc
-;"lairA xp02" = tnof.xtc
 
-;)thgieh.savnac ,htdiw.savnac ,0 ,0(tceRllif.xtc
-;"kcalb" = elytSllif.xtc
+function moveDown() {
+    // Find the index of the top number in the numbers array
+    const topNumberIndex = numbers.indexOf(topNumberValue);
 
-;)"d2"(txetnoCteg.savnac = xtc tsnoc
-{)neercSniam(fi esle
+    // Check if the top number is at the bottom row or there is a filled cell below
+    const isAtBottomRow = topNumberIndex >= (gridHeight - 1) * gridSize;
+    const cellBelowIndex = topNumberIndex + gridSize;
+    const isCellBelowFilled = numbers[cellBelowIndex] !== undefined;
+
+    // Stop moving down if the top number is at the bottom row or there is a filled cell below
+    if (isAtBottomRow || isCellBelowFilled) {
+        //generateNewTopNumber(); // Generate a new top number after reaching the bottom or encountering a filled cell
+        return;
+    }
+
+    // Calculate the destination index for the top number to move one row down in the target column
+    let destinationIndex = topNumberIndex;
+    while (destinationIndex + gridSize < totalCells && numbers[destinationIndex + gridSize] === undefined) {
+        destinationIndex += gridSize;
+    }
+
+    // Move the top number to the next index (down to the target column)
+    numbers[destinationIndex] = topNumberValue;
+    numbers[topNumberIndex] = undefined;
+    drawGrid();
+
+    // Continue the animation after a short delay
+    //setTimeout(moveDown, 200);
+}
+/*
+function animateMoveDown(startIndex, endIndex) {
+    const animationSpeed = 5; // Adjust this value to control the speed of movement
+    const distance = endIndex - startIndex;
+    const direction = distance > 0 ? 1 : -1; // 1 for moving down, -1 for moving up
+
+    const nextIndex = startIndex + direction * animationSpeed;
+
+    // Move the top number to the next index
+    numbers[nextIndex] = topNumberValue;
+    numbers[startIndex] = undefined;
+    drawGrid();
+
+    // Continue the animation until reaching the destination index
+    if ((direction === 1 && nextIndex < endIndex) || (direction === -1 && nextIndex > endIndex)) {
+        requestAnimationFrame(() => animateMoveDown(nextIndex, endIndex));
+    } else {
+        generateNewTopNumber(); // Generate a new top number after the animation is complete
+    }
 }
 
-;)(rebmuNtxeNward    
-}    
-}        
-}            
-;)2 / eziStcer + y ,2 / eziStcer + x ,)(gnirtSot.rebmun(txeTllif.xtc                
-;"elddim" = enilesaBtxet.xtc                
-;"retnec" = ngilAtxet.xtc                
-;"lairA xp02" = tnof.xtc                
-;"der" : ]1 - rebmun[sroloc ? )"rebmun" === rebmun foepyt( = elytSllif.xtc                
-{ )denifednu ==! rebmun( fi            
+function animateMoveDown(startIndex, endIndex) {
+    const animationSpeed = 5; // Adjust this value to control the speed of movement
+    const distance = endIndex - startIndex;
+    const direction = distance > 0 ? 1 : -1; // 1 for moving down, -1 for moving up
 
-}            
-;)(llif.xtc                
-;"orobsniag" = elytSllif.xtc                
-;)IP.htaM * 2 ,0 ,2 / eziStcer ,2 / eziStcer + y ,2 / eziStcer + x(cra.xtc                
-;)(htaPnigeb.xtc                
-{ )"-" === rebmun || "=" === rebmun( fi esle            
-}            
-;)(llif.xtc                
-;"orobsniag" = elytSllif.xtc                
-;)IP.htaM * 2 ,0 ,2 / eziStcer ,2 / eziStcer + y ,2 / eziStcer + x(cra.xtc                
-;)(htaPnigeb.xtc                
-{ ))7 =< rebmun && 1 => rebmun && "rebmun" === rebmun foepyt( && denifednu ==! rebmun( fi            
-;]loc + eziSdirg * wor[srebmun = rebmun tsnoc            
+    const nextIndex = startIndex + direction * animationSpeed;
 
-;eziStcer * wor = y tsnoc            
-;eziStcer * loc = x tsnoc            
-;)eziSdirg+"=eziSdirg"(gol.elosnoc            
-{ )++loc ;eziSdirg < loc ;0 = loc tel( rof        
-;)thgieHdirg+"=thgieHdirg"(gol.elosnoc        
-{ )++wor ;thgieHdirg < wor ;0 = wor tel( rof    
+    // Move the top number to the next index
+    numbers[nextIndex] = topNumberValue;
+    numbers[startIndex] = undefined;
+    drawGrid();
 
-;)thgieh.savnac ,htdiw.savnac ,0 ,0(tceRraelc.xtc
-{)emaGni(fi
-{ )(dirGward noitcnuf
-}
-;)"nward rebmuNtxen"(gol.elosnoc    
-;">3h/<"+eulaVrebmuNweNpot+" :txeN>3h<" = LMTHrenni.tnemelErebmuNtxen    
-;)'rebmuNtxen'(dIyBtnemelEteg.tnemucod = tnemelErebmuNtxen tsnoc    
-{)(rebmuNtxeNward noitcnuf
+    // Continue the animation until reaching the destination index
+    if ((direction === 1 && nextIndex < endIndex) || (direction === -1 && nextIndex > endIndex)) {
+        requestAnimationFrame(() => animateMoveDown(nextIndex, endIndex));
+    } else {
+        generateNewTopNumber(); // Generate a new top number after the animation is complete
+    }
+}*/
+
+
--- touchGesture.js
+++ touchGesture.js
@@ -1,112 +1,111 @@
-<!DOCTYPE html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="generator" content="RocketCake"><title></title></head><style>a { color:#5D5D9E; } a:visited { color:#5D5D9E; } a:active { color:#5C615E; } a:hover { color:#B2B3B4; } a.wsp48fac406{ color:#0080FF; text-decoration: none} a.wsp48fac406:visited{ color:#0080FF; text-decoration: none} a.wsp48fac406:active{ color:#8018CB; text-decoration: underline} a.wsp48fac406:hover{ color:#0000A0; text-decoration: underline} a.wsp63851a2{ color:#FFFFFF; text-decoration: none} a.wsp63851a2:visited{ color:#FFFFFF; text-decoration: none} a.wsp63851a2:active{ color:#FFFFFF; text-decoration: underline} a.wsp63851a2:hover{ color:#C0C0C0; text-decoration: underline} body { background-color:#FFFFFF; padding:0;  margin: 0; }.textstyle1 { text-align:left; }.textstyle2 { font-size:12pt; font-family:Arial, Helvetica, sans-serif; color:#000000;  }.textstyle3 { text-align:center; }#container_78bff89b { vertical-align: top; position:relative; display: inline-block; width:100%; min-height:150px; max-width:700px; background:none;  }#container_78bff89b_padding { margin: 30px; display: block;  }.textstyle4 { font-size:12pt; font-family:Arial, Helvetica, sans-serif; color:#000000; line-height: 1.5;  }.textstyle5 { font-size:22pt; font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; color:#303030; line-height: 1.5;  }.textstyle6 { font-size:12pt; font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; color:#000000; line-height: 1.5;  }.textstyle7 { font-size:12pt; font-family:'Lucida Sans Unicode', 'Lucida Grande', sans-serif; color:#303030; line-height: 1.5;  }#container_365f079f { vertical-align: top; position:relative; display: inline-block; width:100%; min-height:26px; background-color:#C0C0C0;  }#container_365f079f_padding { margin: 10px; display: block;  }.textstyle8 { text-align:right; }.textstyle9 { font-size:10pt; font-family:Arial, Helvetica, sans-serif; color:#1D1D1D;  }</style><body><div class="textstyle1"><span class="textstyle2">  </span></div><div class="textstyle3"><div id="container_78bff89b"><div id="container_78bff89b_padding" ><div class="textstyle3"><span class="textstyle4"><br/><br/></span><span class="textstyle5">Encrypted Website</span><span class="textstyle6"><br/><br/>Your browser actually cannot show this page.</span><span class="textstyle7"><br/><br/></span><span class="textstyle7"><a href="mailto:echkourine@icloud.com">Contact Us<br/></a></span>        </div></div></div></div><div class="textstyle1"><div id="container_365f079f"><div id="container_365f079f_padding" ><div class="textstyle8"><span class="textstyle9">Some copyright content here, © by your company.</span></div></div></div></div></body><div style="display: none;">
+const gameContainer = document.getElementById("gridContainer");
+let touchStartX = 0;
+let touchStartY = 0;
 
+gameContainer.addEventListener("touchstart", handleTouchStart);
+gameContainer.addEventListener("touchmove", handleTouchMove);
+gameContainer.addEventListener("touchend", handleTouchEnd);
 
+function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
+    touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    event.preventDefault(); // Prevents scrolling when dragging on the game container
+    // You can add additional logic here to handle touch movement if needed
+}
+
+function handleTouchEnd(event) {
+    const touchEndX = event.changedTouches[0].clientX;
+    const touchEndY = event.changedTouches[0].clientY;
+
+    // Calculate the touch distance in the X and Y directions
+    const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY;
+
+    // Determine the type of touch gesture based on the distance
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        // Horizontal gesture (left or right)
+        if (deltaX > 0) {
+            // Right swipe
+            // Handle the right swipe action in your game logic
+            actionRight();
+            console.log("Right Swipe");
+        } else {
+            // Left swipe
+            // Handle the left swipe action in your game logic
+            actionLeft();
+            console.log("Left Swipe");
+        }
+    } else {
+        // Vertical gesture (up or down)
+        if (deltaY > 0) {
+		actionDown();
+        } else {
+            // Up swipe
+            // Handle the up swipe action in your game logic
+            console.log("Up Swipe");
+        }
+   }
+}
+
+function actionLeft(){
+    // Move the top number to the left column if it is at the top of the grid
+    if (topNumberIndex > 0) {
+        numbers[topNumberIndex] = undefined;
+        topNumberIndex--;
+        targetColumn--;
+        numbers[topNumberIndex] = topNumberValue;
+        drawGrid();
+    }
+}
+
+function actionRight(){
+    // Move the top number to the right column if it is at the top of the grid
+    if (topNumberIndex < gridSize - 1) {
+        numbers[topNumberIndex] = undefined;
+        topNumberIndex++;
+        targetColumn++;
+        numbers[topNumberIndex] = topNumberValue;
+        drawGrid();
+    }
+}
+function actionDown(){
+	//screenSwitch();
+        console.log("down pressed");
+	if(inGame==true){
+        downArrowCount++;
+        // Move the top number down when the "Arrow Down" key is pressed
+        if(downArrowCount === 7){
+            moveDown();
+            pushLinesUp();
+            gameOver();
+            downArrowCount=0;
+        }
+        else {
+            moveDown();
+            gameOver();
+        }
+        score(numbers);
+    }else {
+	inGame=true;
+	drawGrid();
+	}
+}
+function handleInput(event) {
+    const key = event.key;
+
+    if (key === "ArrowLeft") {
+        actionLeft();
+        console.log("left pressed");
+    } else if (key === "ArrowRight") {
+        actionRight();
+        console.log("right pressed");
+    } else if (key === "ArrowDown") {
+
+	actionDown();
 }
 }
-;)(nwoDnoitca	
 
-{ )"nwoDworrA" === yek( fi esle }    
-;)"desserp thgir"(gol.elosnoc        
-;)(thgiRnoitca        
-{ )"thgiRworrA" === yek( fi esle }    
-;)"desserp tfel"(gol.elosnoc        
-;)(tfeLnoitca        
-{ )"tfeLworrA" === yek( fi    
 
-;yek.tneve = yek tsnoc    
-{ )tneve(tupnIeldnah noitcnuf
-}
-}	
-;)(dirGward	
-;eurt=emaGni	
-{ esle}    
-;)srebmun(erocs        
-}        
-;)(revOemag            
-;)(nwoDevom            
-{ esle        
-}        
-;0=tnuoCworrAnwod            
-;)(revOemag            
-;)(pUseniLhsup            
-;)(nwoDevom            
-{)7 === tnuoCworrAnwod(fi        
-desserp si yek "nwoD worrA" eht nehw nwod rebmun pot eht evoM //        
-;++tnuoCworrAnwod        
-{)eurt==emaGni(fi	
-;)"desserp nwod"(gol.elosnoc        
-;)(hctiwSneercs//	
-{)(nwoDnoitca noitcnuf
-}
-}    
-;)(dirGward        
-;eulaVrebmuNpot = ]xednIrebmuNpot[srebmun        
-;++nmuloCtegrat        
-;++xednIrebmuNpot        
-;denifednu = ]xednIrebmuNpot[srebmun        
-{ )1 - eziSdirg < xednIrebmuNpot( fi    
-dirg eht fo pot eht ta si ti fi nmuloc thgir eht ot rebmun pot eht evoM //    
-{)(thgiRnoitca noitcnuf
-
-}
-}    
-;)(dirGward        
-;eulaVrebmuNpot = ]xednIrebmuNpot[srebmun        
-;--nmuloCtegrat        
-;--xednIrebmuNpot        
-;denifednu = ]xednIrebmuNpot[srebmun        
-{ )0 > xednIrebmuNpot( fi    
-dirg eht fo pot eht ta si ti fi nmuloc tfel eht ot rebmun pot eht evoM //    
-{)(tfeLnoitca noitcnuf
-
-}
-}   
-}        
-;)"epiwS pU"(gol.elosnoc            
-cigol emag ruoy ni noitca epiws pu eht eldnaH //            
-epiws pU //            
-{ esle }        
-;)(nwoDnoitca		
-{ )0 > Yatled( fi        
-)nwod ro pu( erutseg lacitreV //        
-{ esle }    
-}        
-;)"epiwS tfeL"(gol.elosnoc            
-;)(tfeLnoitca            
-cigol emag ruoy ni noitca epiws tfel eht eldnaH //            
-epiws tfeL //            
-{ esle }        
-;)"epiwS thgiR"(gol.elosnoc            
-;)(thgiRnoitca            
-cigol emag ruoy ni noitca epiws thgir eht eldnaH //            
-epiws thgiR //            
-{ )0 > Xatled( fi        
-)thgir ro tfel( erutseg latnoziroH //        
-{ ))Yatled(sba.htaM > )Xatled(sba.htaM( fi    
-ecnatsid eht no desab erutseg hcuot fo epyt eht enimreteD //    
-
-;YtratShcuot - YdnEhcuot = Yatled tsnoc    
-;XtratShcuot - XdnEhcuot = Xatled tsnoc    
-snoitcerid Y dna X eht ni ecnatsid hcuot eht etaluclaC //    
-
-;Ytneilc.]0[sehcuoTdegnahc.tneve = YdnEhcuot tsnoc    
-;Xtneilc.]0[sehcuoTdegnahc.tneve = XdnEhcuot tsnoc    
-{ )tneve(dnEhcuoTeldnah noitcnuf
-
-}
-dedeen fi tnemevom hcuot eldnah ot ereh cigol lanoitidda dda nac uoY //    
-reniatnoc emag eht no gniggard nehw gnillorcs stneverP // ;)(tluafeDtneverp.tneve    
-{ )tneve(evoMhcuoTeldnah noitcnuf
-
-}
-;Ytneilc.]0[sehcuot.tneve = YtratShcuot    
-;Xtneilc.]0[sehcuot.tneve = XtratShcuot    
-{ )tneve(tratShcuoTeldnah noitcnuf
-
-;)dnEhcuoTeldnah ,"dnehcuot"(renetsiLtnevEdda.reniatnoCemag
-;)evoMhcuoTeldnah ,"evomhcuot"(renetsiLtnevEdda.reniatnoCemag
-;)tratShcuoTeldnah ,"tratshcuot"(renetsiLtnevEdda.reniatnoCemag
-
-;0 = YtratShcuot tel
-;0 = XtratShcuot tel
-;)"reniatnoCdirg"(dIyBtnemelEteg.tnemucod = reniatnoCemag tsnoc
