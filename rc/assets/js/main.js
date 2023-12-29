027ea3b58acaf59173b2384096a0e0038c366290
echkourine25 <61584703+echkourine25@users.noreply.github.com>
2023-12-29 12:04:54 +0100

    Update main.js

--- rc/assets/js/main.js
+++ rc/assets/js/main.js
@@ -106,45 +106,52 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-    function updateAgenda() {
-        var agendaItems = [
-            { dateTime: '2023-01-01T09:00:00', event: 'New Year\'s Meeting' },
-            { dateTime: '2023-01-15T12:30:00', event: 'Lunch Break' },
-            { dateTime: '2023-12-29T10:00:00', event: 'Team Meeting' },
-            // Add more agenda items as needed
-        ];
+function updateAgenda() {
+    var agendaItems = [
+        { dateTime: '2023-01-01T09:00:00', event: 'New Year\'s Meeting' },
+        { dateTime: '2023-01-15T12:30:00', event: 'Lunch Break' },
+        { dateTime: '2023-12-29T10:00:00', event: 'Team Meeting' },
+        // Add more agenda items as needed
+    ];
 
-        var agendaBody = document.getElementById('agendaBody');
-        agendaBody.innerHTML = ''; // Clear existing content
-
-        var hours = Array.from({ length: 24 }, (_, i) => i); // Create an array from 0 to 23 representing hours
-
-        hours.forEach(hour => {
-            var newRow = agendaBody.insertRow();
-            newRow.insertCell(0).textContent = hour + ':00'; // Time column
-
-            for (var day = 1; day <= 5; day++) {
-                var cell = newRow.insertCell(day);
-                var eventsForHourAndDay = agendaItems.filter(item => {
-                    var eventDate = new Date(item.dateTime);
-                    return eventDate.getHours() === hour && eventDate.getDay() === day;
-                });
-
-                eventsForHourAndDay.forEach(event => {
-                    cell.textContent += event.event + '\n';
-                });
-            }
-        });
+    // Retrieve agenda events from cookies
+    var storedAgenda = getCookie('agendaEvents');
+    if (storedAgenda) {
+        agendaItems = JSON.parse(storedAgenda);
     }
 
-    // ... existing code ...
+    var agendaBody = document.getElementById('agendaBody');
+    agendaBody.innerHTML = ''; // Clear existing content
 
-    // Call the updateAgenda function to initially populate the agenda
-    updateAgenda();
+    var hours = Array.from({ length: 24 }, (_, i) => i); // Create an array from 0 to 23 representing hours
 
-    // Update the agenda every minute (adjust the interval as needed)
-    setInterval(updateAgenda, 60000); // 60000 milliseconds = 1 minute
+    hours.forEach(hour => {
+        var newRow = agendaBody.insertRow();
+        newRow.insertCell(0).textContent = hour + ':00'; // Time column
 
+        for (var day = 1; day <= 5; day++) {
+            var cell = newRow.insertCell(day);
+            var eventsForHourAndDay = agendaItems.filter(item => {
+                var eventDate = new Date(item.dateTime);
+                return eventDate.getHours() === hour && eventDate.getDay() === day;
+            });
+
+            eventsForHourAndDay.forEach(event => {
+                cell.textContent += event.event + '\n';
+            });
+        }
+    });
+}
+
+// ... existing code ...
+
+// Call the updateAgenda function to initially populate the agenda
+updateAgenda();
+
+// Update the agenda every minute (adjust the interval as needed)
+setInterval(updateAgenda, 60000); // 60000 milliseconds = 1 minute
+
+// ... existing code ...
     function performSearch() {
         // Récupère les valeurs du formulaire
         var query = document.getElementById('searchQuery').value;
