6f30e664bde47af091cd9a11019d5bb54b2d2876
echkourine25 <61584703+echkourine25@users.noreply.github.com>
2023-12-29 13:18:31 +0100

    Update main.js

--- rc/assets/js/main.js
+++ rc/assets/js/main.js
@@ -212,3 +212,13 @@ function addAgendaEvent(){
       descriptionAgenda = document.getElementById("descriptionAgenda").value;
       addEventToAgenda(datetimeAgenda, descriptionAgenda);
 }
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') cookie = cookie.substring(1, cookie.length);
+        if (cookie.indexOf(nameEQ) === 0) return cookie.substring(nameEQ.length, cookie.length);
+    }
+    return null;
+}
